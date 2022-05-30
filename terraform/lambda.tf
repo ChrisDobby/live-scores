d08d8eb5270cf956a0c28113b88b65bdaee1e203
Chris Dobson <chris.dobson@dazn.com>
2022-05-30 21:30:57 +0100

    terraform update

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,13 +1,15 @@
 resource "aws_lambda_function" "get-scorecard-urls" {
-  s3_bucket     = aws_s3_bucket.get-urls-dist.bucket
-  s3_key        = aws_s3_object.get-urls-zip.key
-  function_name = "get-scorecard-urls"
-  handler       = "lib/index.handler"
-  role          = aws_iam_role.get-scorecard-urls-role.arn
+  function_name    = "get-scorecard-urls"
+  handler          = "lib/index.handler"
+  filename         = "../functions/dist/get-scorecard-urls.zip"
+  source_code_hash = filebase64sha256("../functions/dist/get-scorecard-urls.zip")
+  role             = aws_iam_role.get-scorecard-urls-role.arn
 
   runtime     = "nodejs14.x"
   timeout     = 600
   memory_size = 256
+
+  layers = ["arn:aws:lambda:eu-west-2:604776666101:layer:chrome-aws-lambda:1"]
 }
 
 resource "aws_lambda_function" "create-processors" {
