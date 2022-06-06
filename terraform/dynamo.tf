5b60963eb09e71841015ffb0dc5fc5bd2afc2058
Chris Dobson <chris.dobson@dazn.com>
2022-06-06 20:00:03 +0100

    add table for websocket connections

--- terraform/dynamo.tf
+++ terraform/dynamo.tf
@@ -15,3 +15,19 @@ resource "aws_dynamodb_table" "live-score-urls" {
     enabled        = true
   }
 }
+
+resource "aws_dynamodb_table" "live-score-connections" {
+  name         = "cleckheaton-cc-live-score-connections"
+  hash_key     = "connectionId"
+  billing_mode = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "connectionId"
+    type = "S"
+  }
+
+  ttl {
+    attribute_name = "expiry"
+    enabled        = true
+  }
+}
