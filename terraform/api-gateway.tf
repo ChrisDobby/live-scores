c97df154623da1b9fe5ac950a7ef05d43a458197
Chris Dobson <chris.dobson@dazn.com>
2022-06-10 21:46:45 +0100

    add socket-connect lambda

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -6,7 +6,12 @@ resource "aws_apigatewayv2_api" "live-scores" {
 
 resource "aws_apigatewayv2_integration" "live-scores-connect" {
   api_id           = aws_apigatewayv2_api.live-scores.id
-  integration_type = "MOCK"
+  integration_type = "AWS"
+
+  connection_type           = "INTERNET"
+  content_handling_strategy = "CONVERT_TO_TEXT"
+  integration_uri           = aws_lambda_function.socket-connect.invoke_arn
+  passthrough_behavior      = "WHEN_NO_MATCH"
 }
 
 resource "aws_apigatewayv2_integration" "live-scores-disconnect" {
