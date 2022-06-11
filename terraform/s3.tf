fc9216cf3b7b02f0e37e2bf15cc7c20a98f304b0
Chris Dobson <chris.dobson@dazn.com>
2022-06-11 19:56:11 +0100

    create scorecard as public readable s3 object

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -2,11 +2,13 @@ resource "aws_s3_bucket" "scorecards" {
   bucket = "cleckheaton-cc-live-scorecards"
 }
 
-resource "aws_s3_bucket_cors_configuration" "scorecards" {
+resource "aws_s3_bucket_lifecycle_configuration" "scorecards" {
   bucket = aws_s3_bucket.scorecards.bucket
-
-  cors_rule {
-    allowed_methods = ["GET"]
-    allowed_origins = ["*"]
+  rule {
+    id     = "delete-old-scorecards"
+    status = "Enabled"
+    expiration {
+      days = 1
+    }
   }
 }
