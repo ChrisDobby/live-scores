cb6995b8ebffe764ce366433774af1591c4b7b9a
Chris Dobson <chris.dobson@dazn.com>
2022-06-11 10:41:33 +0100

    add socket-disconnect lambda

---
+++ functions/socket-disconnect/src/index.ts
@@ -0,0 +1,14 @@
+import { DynamoDB } from 'aws-sdk'
+
+const dynamoClient = new DynamoDB({ region: 'eu-west-2' })
+const TableName = 'cleckheaton-cc-live-score-connections'
+
+export const handler = async event => {
+  const { connectionId } = event.requestContext
+  await dynamoClient
+    .deleteItem({
+      TableName,
+      Key: { connectionId: { S: connectionId } },
+    })
+    .promise()
+}
--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -16,7 +16,12 @@ resource "aws_apigatewayv2_integration" "live-scores-connect" {
 
 resource "aws_apigatewayv2_integration" "live-scores-disconnect" {
   api_id           = aws_apigatewayv2_api.live-scores.id
-  integration_type = "MOCK"
+  integration_type = "AWS"
+
+  connection_type           = "INTERNET"
+  content_handling_strategy = "CONVERT_TO_TEXT"
+  integration_uri           = aws_lambda_function.socket-disconnect.invoke_arn
+  passthrough_behavior      = "WHEN_NO_MATCH"
 }
 
 resource "aws_apigatewayv2_route" "live-scores-connect" {
