7a7f6cd4ce0ed411a1d0c144db9adb3337b4c62e
Chris Dobson <chris.dobson@dazn.com>
2022-06-11 10:45:35 +0100

    update scorecard bucket cors config

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,3 +1,12 @@
 resource "aws_s3_bucket" "scorecards" {
   bucket = "cleckheaton-cc-live-scorecards"
 }
+
+resource "aws_s3_bucket_cors_configuration" "scorecards" {
+  bucket = aws_s3_bucket.scorecards.bucket
+
+  cors_rule {
+    allowed_methods = ["GET"]
+    allowed_origins = ["*"]
+  }
+}
