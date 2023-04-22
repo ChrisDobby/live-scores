802125e749df3fefbfb0be9e5645b3043b18513f
Chris Dobson <chrd@trustpilot.com>
2023-04-22 21:44:22 +0100

    allow sns to send to sqs

--- terraform/sqs.tf
+++ terraform/sqs.tf
@@ -15,3 +15,28 @@ resource "aws_sqs_queue" "web-notify" {
   message_retention_seconds  = 300
   visibility_timeout_seconds = 60
 }
+
+data "aws_iam_policy_document" "web-notify" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "service"
+      identifiers = "sns.amazonaws.com"
+    }
+
+    actions   = ["sqs:SendMessage"]
+    resources = [aws_sqs_queue.web-notify.arn]
+
+    condition {
+      test     = "ArnEquals"
+      variable = "aws:SourceArn"
+      values   = [aws_sns_topic.push-notification.arn]
+    }
+  }
+}
+
+resource "aws_sqs_queue_policy" "web-notify" {
+  queue_url = aws_sqs_queue.web-notify.id
+  policy    = data.aws_iam_policy_document.web-notify.json
+}
