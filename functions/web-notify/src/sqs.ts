9e7de3e7854f3a0ba63bc056a7d0ff92d10c7277
Chris Dobson <chrd@trustpilot.com>
2023-05-06 20:37:32 +0100

    log errors when sending delete sub msg

--- functions/web-notify/src/sqs.ts
+++ functions/web-notify/src/sqs.ts
@@ -5,10 +5,14 @@ const deleteSubscriptionQueueUrl = `${process.env.DELETE_SUBSCRIPTION_QUEUE_URL}
 const sqsClient = new SQSClient({ region: 'eu-west-2' });
 
 export const sendDeleteSubscriptionMessage = async (endpoint: string) => {
-  await sqsClient.send(
-    new SendMessageCommand({
-      QueueUrl: deleteSubscriptionQueueUrl,
-      MessageBody: JSON.stringify({ endpoint }),
-    }),
-  );
+  try {
+    await sqsClient.send(
+      new SendMessageCommand({
+        QueueUrl: deleteSubscriptionQueueUrl,
+        MessageBody: JSON.stringify({ endpoint }),
+      }),
+    );
+  } catch (error) {
+    console.error('Error sending delete subscription message', error);
+  }
 };
