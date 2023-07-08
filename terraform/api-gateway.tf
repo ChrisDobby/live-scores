da5c2b911412ed59793502816805a976a7f52280
Chris Dobson <chrd@trustpilot.com>
2023-07-08 21:24:32 +0100

    integration method

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -104,7 +104,7 @@ resource "aws_apigatewayv2_integration" "notifications-unsubscribe" {
 
   connection_type      = "INTERNET"
   integration_uri      = module.subscribe-to-scores.invoke_arn
-  integration_method   = "DELETE"
+  integration_method   = "POST"
   passthrough_behavior = "WHEN_NO_MATCH"
 }
 
@@ -123,7 +123,7 @@ resource "aws_apigatewayv2_integration" "notifications-update" {
 
   connection_type      = "INTERNET"
   integration_uri      = module.subscribe-to-scores.invoke_arn
-  integration_method   = "PUT"
+  integration_method   = "POST"
   passthrough_behavior = "WHEN_NO_MATCH"
 }
 
